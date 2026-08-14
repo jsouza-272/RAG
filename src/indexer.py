aca6ea78883f20f86b880d340d35ead9a6641cf9
Joao Souza <jvpvpsouza7@gmail.com>
2026-08-14 23:51:32 +0100

    feat: added chunk split function

--- src/indexer.py
+++ src/indexer.py
@@ -2,6 +2,7 @@ import ast
 import os
 import re
 import json
+import math
 from .models import MinimalSource
 from pathlib import Path
 
@@ -18,6 +19,56 @@ def get_vllm_path() -> Path:
     path = Path(vllm_path)
     if not path.is_dir():
         raise FileNotFoundError("vllm is not directory")
+    '''if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")
+    if not path.is_dir():
+        raise FileNotFoundError("vllm is not directory")'''
     return path
 
 
@@ -40,19 +91,69 @@ def dump_chunks(py_chunks: list[MinimalSource],
         json.dump([c.dump() for c in py_chunks + md_chunks], file)
 
 
-def chunk_python_file(file_path: str) -> None:
+def build_index_by_line(source: str) -> list[int]:
+    line_start_offsets = [0]
+    for line in source.splitlines(keepends=True):
+        line_start_offsets.append(line_start_offsets[-1] + len(line))
+    return line_start_offsets
+
+
+def chunk_python_file(file_path: str,
+                      max_chunk_size: int) -> list[MinimalSource]:
+    chunks_list: list[MinimalSource] = []
+    current_chunks: list[MinimalSource] = []
     code = ""
     with open(file_path) as file:
         code = file.read()
+    start_offsets = build_index_by_line(code)
     ast_file = ast.parse(code)
-    chunk = ""
-    for line in ast_file:
+    generic = False
+    start_index = 0
+    end_index = 0
+    for line in ast_file.body:
+        current_chunks = []
         if isinstance(line, ast.ClassDef):
-            chunk = ast.get_source_segment(code, line)
+            generic = False
+            start_index = start_offsets[line.lineno - 1]
+            end_index = start_offsets[
+                line.end_lineno - 1] + line.end_col_offset
         elif isinstance(line, (ast.FunctionDef, ast.AsyncFunctionDef)):
-            chunk = ast.get_source_segment(code, line)
+            generic = False
+            start_index = start_offsets[line.lineno - 1]
+            end_index = start_offsets[
+                line.end_lineno - 1] + line.end_col_offset
         else:
-            # fazer flag para guardar o comeco desse bloco generico
-            pass
-        chunk
-        # remover linha acima
+            if not generic:
+                start_index = start_offsets[line.lineno - 1]
+            generic = True
+            end_index = start_offsets[
+                line.end_lineno - 1] + line.end_col_offset
+        current_chunks.append({"first_character_index": start_index,
+                               "last_character_index": end_index})
+        if end_index - start_index > max_chunk_size:
+            current_chunks = split_chunk(
+                max_chunk_size, int(max_chunk_size * 0.1),
+                start_index, end_index)
+        chunks_list.extend(MinimalSource(file_path=file_path, **chunk)
+                           for chunk in current_chunks)
+        print("\n\033[38;2;200;0;200m", "="*100, "\033[0m\n")
+        print(start_index, end_index)
+        print("got:", code[start_index:end_index])
+        print()
+        print("expected:", ast.get_source_segment(code, line))
+    print("\n\033[38;2;200;0;200m", "="*100, "\033[0m\n")
+    return chunks_list
+
+
+def split_chunk(max_chunk_size: int, overlap_size: int,
+                current_start: int, current_end: int) -> dict:
+    step = max_chunk_size - overlap_size
+    min_chunks = math.ceil((current_end - current_start) / step)
+    chunks: list[dict] = []
+    for i in range(min_chunks):
+        chunk_info = {"first_character_index": current_start,
+                      "last_character_index": min(
+                          current_start + max_chunk_size, current_end)}
+        current_start += step
+        chunks.append(chunk_info)
+    return chunks
--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 import fire
+import os
 from .cli_handler import StudentCLI
-from .indexer import get_vllm_path, get_all_files
+from .indexer import get_vllm_path, get_all_files, chunk_python_file
 
 
 def main():
-    fire.Fire(component=StudentCLI, name="uv run python3 -m src")
-    vllm_path = get_vllm_path()
-    print(vllm_path)
-    python_files = get_all_files(vllm_path, ".py")
-    print(python_files)
-    print("\033[38;2;255;0;255m", "="*200, "\033[0m")
-    md_files = get_all_files(vllm_path, ".md")
-    print(md_files)
+    os.system("clear")
+    #fire.Fire(component=StudentCLI, name="uv run python3 -m src")
+    #vllm_path = get_vllm_path()
+    #print(vllm_path)
+    #python_files = get_all_files(vllm_path, ".py")
+    #print(python_files)
+    #print("\033[38;2;255;0;255m", "="*200, "\033[0m")
+    #md_files = get_all_files(vllm_path, ".md")
+    #print(md_files)
+    print(chunk_python_file("src/indexer.py", 2000))
